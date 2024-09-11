209df82906c7e65857e214a15353b7bc1652c545
kevin <kipronok234@gmail.com>
2024-09-11 16:06:36 +0300

    python-p3-v2mapping-python

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,35 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
-import ipdb
+import  ipdb
+
+Department.drop_table()
+Department.create_table()
+
+# payroll = Department("Payroll", "Building A, 5th Floor")
+# print(payroll)  # <Department None: Payroll, Building A, 5th Floor>
+
+# payroll.save()  # Persist to db, assign object id attribute
+# print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+# hr = Department("Human Resources", "Building C, East Wing")
+# print(hr)  # <Department None: Human Resources, Building C, East Wing>
+
+# hr.save()  # Persist to db, assign object id attribute
+# print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)  # <Department 2: HR, Building F, 10th Floor>
+
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll)  
 
 ipdb.set_trace()
